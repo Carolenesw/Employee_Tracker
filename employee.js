3c5c19b634e4fca766e49b14951242d68e364941
Carolene Wilson <carolenesw@gmail.com>
2020-07-15 14:19:29 -0400

    create function to prompt employee query/selection

--- employee.js
+++ employee.js
@@ -13,9 +13,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
+// set up connection to sql server and database
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -24,17 +22,43 @@ const connection = mysql.createConnection({
   database: "employee_db"
 });
 
+// create function to handle connection 
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
-    return;
+   
+    // get employee's data after connection is made
+    getData()
   }
 
   console.log("connected as id " + connection.threadId);
 });
 
+// create function to prompt employee query/selection
+function getData() {
+    inquirer
+      .prompt({
+        name: "query",
+        type: "list",
+        message: "Please select action to be taken?",
+        choices: ["View All Employees", 
+                  "View All Departments", 
+                  "View Employee Roles", 
+                  "View Employees By Department",
+                  "View Employees By Manager", 
+                  "View Employees By Role", 
+                  "Add New Employee", 
+                  "Add New Department",
+                  "Add New Role",
+                  "Update/Edit Employee Manager", 
+                  "Update/Edit Employee Role", 
+                  "Delete/Remove Employee",
+                  "View the total utilized Budget of a Department"
+                ]
+      })
+}
 
+// start server 
 app.listen(PORT, function() {
-    // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });
