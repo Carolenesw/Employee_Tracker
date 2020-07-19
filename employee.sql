ee3a3c35d40b734b067c9a27b188f70e2f20b2aa
Carolene Wilson <carolenesw@gmail.com>
2020-07-19 11:48:38 -0400

    populate employees table data

--- employee.sql
+++ employee.sql
@@ -28,3 +28,7 @@ name VARCHAR(30) NOT NULL,
 PRIMARY KEY (emp_id)
 );
 
+-- populate employees table
+SELECT * FROM employee;
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)  VALUES ("John","Morris", 01, 1), ("David","Brown", 05, 2),  ("Christopher","Thompson", 03, 1),("Masha","Matthews", 08, 1),  ("Daniel","Kelly", 02, 2), ("Angela", "Dawkins", 07, 2);
